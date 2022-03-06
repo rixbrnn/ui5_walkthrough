315189f87dfbc0cde8efd3b167eec8fb8ec03873
rixbrnn <richardbringmann160301@gmail.com>
2022-03-06 04:12:35 -0300

    Step 8: Translatable Texts (adding i18n config to the project)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,8 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 
@@ -15,10 +16,22 @@ sap.ui.define([
             };
             const oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            //set i18n model on view
+            const i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
         onShowHello: function (){
+            
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
             // show a native JS alert
-            MessageToast.show("Hello world");
+            MessageToast.show(sMsg);
         }
     });
 });
