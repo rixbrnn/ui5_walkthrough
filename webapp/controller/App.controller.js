95a66251a5dd2834687038d9c8e986e11d9e33af
rixbrnn <richardbringmann160301@gmail.com>
2022-03-06 03:35:43 -0300

    Step 6: Modules (using a module/control instead of native javascript popup)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
         onShowHello: function (){
             // show a native JS alert
-            alert("Hello World!");
+            MessageToast.show("Hello world");
         }
     });
 });
