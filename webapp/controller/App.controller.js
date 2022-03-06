b2258914795fb1e464a91c0e0cae4d63713e0d0c
rixbrnn <richardbringmann160301@gmail.com>
2022-03-06 07:50:57 -0300

    Step 15: Nested Views (changing the hello panel to a separated view)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,16 +4,5 @@ sap.ui.define([
 ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
-        onShowHello: function (){
-            
-            // read msg from i18n model
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-            // show a native JS alert
-            MessageToast.show(sMsg);
-        }
     });
 });
